16b1b15812aa869586f737873e667b508523650f
Thomas Walsh <22walsht@gmail.com>
2025-06-20 13:15:28 +0100

    Renaming project files

--- addons/strategy_camera/strategy_camera_autoload.gd
+++ addons/strategy_camera/strategy_camera_autoload.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("StrategyCamera", "Camera2D", preload("strategy_camera.gd"), preload("icon_camera_grid.svg"))
+	add_custom_type("StrategyCamera", "Camera2D", preload("strategy_camera.gd"), preload("IconCameraGrid.svg"))
 
 
 func _exit_tree() -> void:
